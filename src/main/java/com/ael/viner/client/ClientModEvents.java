c1006653577702703735577047f7517a1d49916c
Jason Harris <jasonharris306@gmail.com>
2023-10-19 16:04:04 +0100

    removed unused code

--- src/main/java/com/ael/viner/client/ClientModEvents.java
+++ src/main/java/com/ael/viner/client/ClientModEvents.java
@@ -34,8 +34,6 @@ public class ClientModEvents {
         );
     }
 
-
-
     // Event handler for block breaking
     @SubscribeEvent
     public static void onBlockBroken(BlockEvent.BreakEvent event) {
@@ -56,76 +54,4 @@ public class ClientModEvents {
 
     }
 
-//    @SubscribeEvent
-//    public static void onBlockBroken(BlockEvent.BreakEvent event) {
-//
-//        // Getting world instance
-//        Level world = (Level)event.getLevel();
-//
-//        // Exit conditions for server-side and correct event type
-//        if (world.isClientSide() || !(event instanceof BlockEvent.BreakEvent))
-//            return;
-//
-//        // Getting player and tool instances
-//        Player player = event.getPlayer();
-//        ItemStack tool = player.getItemInHand(InteractionHand.MAIN_HAND);
-//
-//        // Exit condition if custom key binding is not pressed
-//        if (!SHIFT_KEY_BINDING.isDown())
-//            return;
-//
-//        // Other variable initializations
-//        BlockPos pos = event.getPos();
-//        BlockState targetBlockState = world.getBlockState(pos);
-//
-//        // Exit condition if block is not veinmineable
-//        if (!VinerBlockRegistry.isVeinmineable(targetBlockState.getBlock()))
-//            return;
-//
-//        // Checking if block can be harvested
-//        boolean canHarvest = targetBlockState.canHarvestBlock(world, pos, player);
-//
-//        if (!canHarvest)
-//            return;
-//
-//        // Initializations for connected block collection
-//        List<BlockPos> connectedBlocks = new ArrayList<>();
-//        Set<BlockPos> visited = new HashSet<>();
-//        collectConnectedBlocks(world, pos, targetBlockState, connectedBlocks, visited);
-//
-//        // Checking for Silk Touch enchantment
-//        boolean hasSilkTouch = EnchantmentHelper.getTagEnchantmentLevel(Enchantments.SILK_TOUCH, tool) > 0;
-//
-//        // Initializing block count
-//        int blockCount = 0;
-//
-//        // Iterating through connected blocks and breaking them
-//        for (BlockPos connectedPos : connectedBlocks) {
-//
-//            // Exit condition if block count reaches veinmineable limit
-//            if (blockCount >= VinerBlockRegistry.getVeinableLimit())
-//                break;
-//
-//            // Getting block state of connected block
-//            BlockState connectedBlockState = world.getBlockState(connectedPos);
-//
-//            // Checking if block is veinmineable and breaking it
-//            if (VinerBlockRegistry.isVeinmineable(targetBlockState.getBlock())) {
-//                if (hasSilkTouch) {
-//                    Block.popResource(world, connectedPos, new ItemStack(connectedBlockState.getBlock()));
-//                } else {
-//                    Block.dropResources(connectedBlockState, world, connectedPos);
-//                }
-//
-//                // Removing block from world
-//                world.removeBlock(connectedPos, false);
-//                blockCount++;
-//            }
-//        }
-//
-//        // Updating tool damage
-//        // TODO: Should we take efficacy into account?
-//        ItemStack item = player.getItemInHand(InteractionHand.MAIN_HAND);
-//        item.setDamageValue(item.getDamageValue() + blockCount);
-//    }
 }
